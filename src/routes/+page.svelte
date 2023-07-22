85255a24abf567b3ebdf61c727a9518ec97e7590
Gian Franco <gianfraback@gmail.com>
2023-07-22 13:40:12 -0500

    refactor: main page cleaned

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import GButton from '$lib/GButton.svelte';
-	import { ButtonFontSize, ButtonTypes } from '../models';
-	import '../global.css';
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<GButton type={ButtonTypes.BUTTON} fontSize={ButtonFontSize.MEDIUM}>Hola mundo</GButton>
