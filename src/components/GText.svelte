e44f325d9d5bcf5d21ab01683facb3e7e1a6cc61
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-07-22 12:51:31 -0500

    feat: add text component

---
+++ src/components/GText.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	type Element = 'p' | 'h1' | 'h2';
+
+	export let variant: Element = 'p';
+</script>
+
+<svelte:element this={variant} class="shared {$$restProps.class || ''}">
+	<slot />
+</svelte:element>
+
+<style>
+	.shared {
+		color: #fff;
+		text-shadow: 0 4px 4px #89024a;
+		font-family: VT323;
+	}
+	h1 {
+		font-size: 4rem;
+	}
+
+	h2 {
+		font-size: 2.25rem;
+	}
+
+	h3 {
+		font-size: 2rem;
+	}
+
+	p {
+		font-weight: 1rem;
+	}
+</style>
