b1fda6e4b49632aea32488f79fa9c76984540c67
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-07-22 12:35:55 -0500

    feat: :art: img zoom
    [feature/gimgzoom]  Add Component GImgZoom
    
    #2

---
+++ src/lib/GImgZoom/GImgZoom.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import styles from './styles/TutiImgZoom.css';;
+</script>
+
+<div class=TutiImgZoom__container>
+  <figure class=TutiImgZoom__figure>
+    <img
+      class=TutiImgZoom__img
+      src="https://i.imgur.com/D3PdaOX.png"
+      alt="Alan"
+    />
+    <figcaption class=TutiImgZoom__figcaption>Alan</figcaption>
+  </figure>
+</div>
+
