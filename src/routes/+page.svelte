05f8ae0100eb419987a8588ad1da3d82da592672
Gian Franco <gianfraback@gmail.com>
2023-07-22 11:53:04 -0500

    feat: print gbutton main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+	import GButton from '$lib/GButton.svelte';
+	import { ButtonTypes } from '../models';
+	import '../global.css';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<GButton type={ButtonTypes.SUBMIT}>Click Aquí</GButton>
