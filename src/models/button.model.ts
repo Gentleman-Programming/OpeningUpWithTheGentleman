7eef1daeca5ac5e1fbf6889ab69cd0b828e3ec47
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-07-22 19:30:50 -0500

    act

---
+++ src/models/button.model.ts
@@ -0,0 +1,9 @@
+export enum ButtonTypes {
+	'SUBMIT' = 'submit',
+	'BUTTON' = 'button'
+}
+export enum ButtonFontSize {
+	'SMALL' = '0.75rem',
+	'MEDIUM' = '1rem',
+	'LARGE' = '2.25rem'
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../reset.css';
+	import '../global.css';
+</script>
+
+<header />
+<main>
+	<slot />
+</main>
+<footer />
+
+<style>
+	main {
+		background-image: url('/img/bg.jpg');
+		background-repeat: repeat;
+		min-height: 100vh;
+		min-height: 100dvh;
+	}
+</style>
